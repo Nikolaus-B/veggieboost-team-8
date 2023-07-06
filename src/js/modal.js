2c834fe3ebb485452c85987a60d9e23f1d7e1ce9
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-07-06 13:43:59 +0300

    add func

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn1: document.querySelector('[data-modal-open-vegetables]'),
+    openModalBtn2: document.querySelector('[data-modal-open-mobile]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
