f26c7a65193e99f6cf8efdb141c2af6fa6ffcabd
SvitLana1990 <svitlana112@outlook.es>
2023-07-06 14:22:39 +0200

    fix-button

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     openModalBtn1: document.querySelector('[data-modal-open-vegetables]'),
     openModalBtn2: document.querySelector('[data-modal-open-mobile]'),
+    openModalBtn3: document.querySelector('[data-modal-open-main]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
@@ -10,6 +11,7 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
